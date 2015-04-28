bee48fae4d4ba9dd4d80cdb0c48c1a0147ffb27e
Maciej Kucharski <maciek.zir+github@gmail.com>
2015-04-28 20:50:22 +0200

    [master]basic gulp - pre-setup

---
+++ gulp/index.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var path = require('path');
+
+module.exports = function registerTasks(tasks) {
+  tasks.forEach(function(task) {
+     gulp.task(task, require(path.join('./tasks', task)));
+  });
+};
+
